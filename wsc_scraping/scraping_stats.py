035f467d97484fae5d622dc5033f802c9324f974
Polonez1 <darjus.vasiukevic@gmail.com>
2023-09-16 23:17:05 +0300

    created wsc href collector

--- wsc_scraping/scraping_stats.py
+++ wsc_scraping/scraping_stats.py
@@ -2,6 +2,17 @@ import pandas as pd
 
 from selenium import webdriver
 from selenium_stealth import stealth
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+
+from bs4 import BeautifulSoup
+
+import pickle
+import time
+from time import sleep
+
+
+import wsc_config
 
 
 class ParsingData:
@@ -11,9 +22,9 @@ class ParsingData:
         self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
         self.options.add_experimental_option("useAutomationExtension", False)
         self.options.add_argument("--disable-notifications")
-        self.driver = webdriver.Chrome(
-            options=self.options, executable_path=r"./wsc_scraping/chromedriver.exe"
-        )
+
+        chrome_service = ChromeService(executable_path=wsc_config.CHROME_DRIVER_V117)
+        self.driver = webdriver.Chrome(service=chrome_service, options=self.options)
         stealth(
             self.driver,
             languages=["en-US", "en"],
@@ -23,11 +34,57 @@ class ParsingData:
             renderer="Intel Iris OpenGL Engine",
             fix_hairline=True,
         )
-        self.headers = {
-            "Accept": "*/*",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
-        }
+        # self.headers = {
+        #    "Accept": "*/*",
+        #    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+        # }
+
+    def get_cookies(self, url: str) -> str("html file"):
+        """Get cookies from url"""
+        self.driver.get(url)
+        name = url.partition(".")[2].partition(".")[0]
+        # time.sleep(30)
+        pickle.dump(self.driver.get_cookies(), open(f"session_{name}", "wb"))
+        # time.sleep(20)
+        self.driver.quit()
+        print("Cookies save")
+
+    def _collect_leagues_hrefs(self):
+        comp = self.driver.find_element(By.ID, "domestic-regions").find_elements(
+            By.CLASS_NAME, "t"
+        )
+        for i in comp:
+            href = i.get_attribute("href")
+            print(href)
+
+    def _leagues_loop(self):
+        self.driver.find_element(By.XPATH, wsc_config.ALL_LEAGUES_BUTTON).click()
+        regions = self.driver.find_element(By.ID, "domestic-index")
+        body_html = regions.get_attribute("innerHTML")
+        soup = BeautifulSoup(body_html, "lxml")
+        all_tags = soup.find_all("a")
+        for i in range(2, len(all_tags) + 1):
+            btn = wsc_config.ALPHABET_BUTTON.format(n=i)
+            self.driver.find_element(By.XPATH, btn).click()
+            self._collect_leagues_hrefs()
+
+            break
+
+    def entry_wsc(self):
+        url = wsc_config.WEB
+        self.driver.get(url)
+        for cookies in pickle.load(open(wsc_config.WSC_COOKIES, "rb")):
+            self.driver.add_cookie(cookies)
+        self.driver.refresh()
+
+        self.driver.find_element(By.XPATH, wsc_config.AGREE_COOKIES_BUTTON).click()
+        self._leagues_loop()
+
+        print("pop up disabled")
+        time.sleep(180)
+        self.driver.quit()
 
 
 if "__main__" == __name__:
     p = ParsingData()
+    p.entry_wsc()
--- wsc_scraping/wsc_config.py
+++ wsc_scraping/wsc_config.py
@@ -1,1 +1,14 @@
+CHROME_DRIVER_V117 = r"./wsc_scraping/chromedriver.exe"
+
+
 WEB = "https://www.whoscored.com/"
+WSC_COOKIES = ".\wsc_scraping\session_whoscored"
+
+
+# Buttons
+
+AGREE_COOKIES_BUTTON = '//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]'
+
+ALL_LEAGUES_BUTTON = '//*[@id="tournament-groups"]/li[3]'
+
+ALPHABET_BUTTON = '//*[@id="domestic-index"]/dd[{n}]'
